ce368ce1c4ed88f4c9bc303720f16d446ce765c0
Federico <federicolucaspontoriero@gmail.com>
2022-06-23 02:36:15 -0300

    paths and tests for the new page

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
 
@@ -13,3 +14,38 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create a new user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
+    
+    def update(self, instance, validated_data):
+        """Update a user, password anr return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        
+        return user
+    
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for user authentication"""
+    email = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+    
+    def validate(self, attrs):
+        """Validate and authenticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+        if not user:
+            msg = _('Unable to authenticate with the credentials')
+            raise serializers.ValidationError(msg, code='authentication')
+        
+        attrs['user'] = user
+        return attrs
